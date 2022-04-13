ccf48d0e54f397c7200db21d954a7224e9e043d1
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-04-12 22:05:06 -0300

    NewContact

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,11 @@
+import ContactForm from '../../components/ContactForm';
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
   return (
-    <div>New Contact</div>
+    <>
+      <PageHeader title="Novo contato" />
+      <ContactForm buttonLabel="Cadastrar" />
+    </>
   );
 }
